a79bbe1ba6859f45d2943deb7db3951a4e0d281d
samirdourao <samir3500@gmail.com>
2022-09-16 11:40:56 -0300

    lógica para mudar o background

--- js/script.js
+++ js/script.js
@@ -1,66 +1,34 @@
 const horas = new Date().toLocaleTimeString()
-console.log(horas)
+const body = document.getElementById("body")
 const holder = document.getElementById("holder")
 const mostraHora = document.createElement("p")
 const imgHolder = document.createElement("figure")
 const imgRef = document.createElement("img")
-
 
 function renderizarDados() {
     mostraHora.innerText = `São exatamente ${horas}`
 
     if (parseInt(horas) >= 0) {
         imgRef.src = "/img/madrugada.png"
+        body.classList.add("madrugada")
     }
 
-    else if (parseInt(horas) >= 6) {
+    if (parseInt(horas) >= 6) {
         imgRef.src = "./img/dia.jpg"
+        body.classList.add("manha")
     }
 
-    else if (parseInt(horas) >= 12) {
+    if (parseInt(horas) >= 12) {
         imgRef.src = "./img/tarde.jpg"
+        body.classList.add("tarde")
     }
 
-    else {
+    if (parseInt(horas) >= 18) {
         imgRef.src = "./img/noite.jpg"
+        body.classList.add("noite")
     }
 
     imgHolder.appendChild(imgRef)
     holder.append(mostraHora, imgHolder)
 }
 renderizarDados()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const dateHours = new Date().toLocaleString()
-// console.log(dateHours)
-
-// const date = new Date().toLocaleDateString()
-// console.log(date)
-
-//Captura e criação de Elementos necessarios
